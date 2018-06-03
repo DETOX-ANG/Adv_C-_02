791f2ee9d0094359536c6d2b169680fc94f526d9
AngeloPC <enriquecido1@gmail.com>
2018-06-03 14:24:15 -0300

    Adv_C#_02_02
    Created extension methods and dynamic values

---
+++ ExtensionMethods/ExtensionString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods {
+    public static class ExtensionString {
+
+        public static string FirstToUpper(this String str) {
+
+            string FirstLetter = str.Substring(0, 1);
+            string SecondLetter = str.Substring(1);
+
+            return FirstLetter.ToUpper() + SecondLetter;
+        }
+    }
+}
---
+++ VarDynamics/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarDynamics {
+    class Program {
+        static void Main(string[] args) {
+
+            //O tipo var tem que ser definido na hora que for instanciado
+            var nome = "Angelo";
+
+            //Dynamic Só é definida na hora da execução. Ao ser declarada, não é apresentado erro de compilação
+            //Porém se o valor for inválido, ela apresentará um erro(exception) na hora da execução
+            dynamic nomeC = "Angelo Rocha";
+        }
+    }
+}
